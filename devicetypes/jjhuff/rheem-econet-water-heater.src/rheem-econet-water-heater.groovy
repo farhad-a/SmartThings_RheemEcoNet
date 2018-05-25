d88a1184af1b99ff60e278e6e7685367fc3babda
Farhad Alaghband <falaghband@gmail.com>
2018-05-25 13:39:29 -0700

    Changed to use multiAttributeTile.

--- devicetypes/jjhuff/rheem-econet-water-heater.src/rheem-econet-water-heater.groovy
+++ devicetypes/jjhuff/rheem-econet-water-heater.src/rheem-econet-water-heater.groovy
@@ -17,94 +17,129 @@
  *  Based on https://github.com/copy-ninja/SmartThings_RheemEcoNet
  */
 metadata {
-	definition (name: "Rheem Econet Water Heater", namespace: "jjhuff", author: "Justin Huff") {
-		capability "Actuator"
-		capability "Refresh"
-		capability "Sensor"
-        capability "Switch"
-		capability "Thermostat Heating Setpoint"
-		
-		command "heatLevelUp"
-		command "heatLevelDown"
-		command "updateDeviceData", ["string"]
-	}
-
-	simulator { }
-
-	tiles {
-		valueTile("heatingSetpoint", "device.heatingSetpoint", inactiveLabel: false, width: 2, height: 2) {
-			state("heatingSetpoint", label:'${currentValue}°',
-				backgroundColors:[
-					[value: 90,  color: "#f49b88"],
-					[value: 100, color: "#f28770"],
-					[value: 110, color: "#f07358"],
-					[value: 120, color: "#ee5f40"],
-					[value: 130, color: "#ec4b28"],
-					[value: 140, color: "#ea3811"]					
-				]
-			)
-		}
-		standardTile("heatLevelUp", "device.switch", canChangeIcon: false, decoration: "flat" ) {
-			state("heatLevelUp",   action:"heatLevelUp",   icon:"st.thermostat.thermostat-up", backgroundColor:"#F7C4BA")
-		}  
-		standardTile("heatLevelDown", "device.switch", canChangeIcon: false, decoration: "flat") {
-			state("heatLevelDown", action:"heatLevelDown", icon:"st.thermostat.thermostat-down", backgroundColor:"#F7C4BA")
-		}
-
-		standardTile("switch", "device.switch", canChangeIcon: false, decoration: "flat" ) {
-       		state "on", label: 'On', action: "switch.off",
-          		icon: "st.switches.switch.on", backgroundColor: "#79b821"
-       		state("off", label: 'Off', action: "switch.on",
-          		icon: "st.switches.switch.off", backgroundColor: "#ffffff")
-		}
-        
-		standardTile("refresh", "device.switch", decoration: "flat") {
-			state("default", action:"refresh.refresh",        icon:"st.secondary.refresh")
-		}
-        
-		main "heatingSetpoint"
-		details(["heatingSetpoint", "heatLevelUp", "heatLevelDown", "switch", "refresh"])
-	}
+  definition (name: "Rheem Econet Water Heater", namespace: "jjhuff", author: "Justin Huff") {
+    capability "Actuator"
+    capability "Refresh"
+    capability "Sensor"
+    capability "Switch"
+    capability "Thermostat Heating Setpoint"
+    
+    command "heatLevelUp"
+    command "heatLevelDown"
+    command "updateDeviceData", ["string"]
+  }
+  
+  simulator { }
+  
+  tiles(scale: 2) {
+    multiAttributeTile(name: "heatingSetpoint", type: "thermostat", width: 6, height: 4) {
+      tileAttribute("device.heatingSetpoint", key: "PRIMARY_CONTROL") {
+        attributeState("heatingSetpoint", label: '${currentValue}', unit:"dF", backgroundColors: getTempColors(), defaultState: true)
+      }
+      /*
+       tileAttribute("device.switch", key: "SECONDARY_CONTROL") {
+       attributeState("off", label: "Off", action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#ffffff", nextState: "turningOn")
+       attributeState("on", label: "On", action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#00a0dc", nextState: "turningOff")
+       attributeState("turningOn", label: "Turning on...", icon: "st.switches.switch.on", backgroundColor: "#00a0dc", nextState: "turningOff")
+       attributeState("turningOff", label: "Turning off...", icon: "st.switches.switch.off", backgroundColor: "#ffffff", nextState: "turningOn")
+       }
+       */
+      tileAttribute("device.switch", key: "OPERATING_STATE") {
+        attributeState("off", label: "Off", backgroundColor: "#cccccc")
+        attributeState("on", label: "On", backgroundColor: "#bc2323", defaultState: true)
+      }
+      tileAttribute("device.heatingSetpoint", key: "VALUE_CONTROL") {
+        attributeState("VALUE_UP", action: "heatLevelUp")
+        attributeState("VALUE_DOWN", action: "heatLevelDown")
+      }
+    }
+    
+    valueTile("heatingSetpointValue", "device.heatingSetpoint", inactiveLabel: false, width: 4, height: 4) {
+      state("heatingSetpoint", label: '${currentValue}\u00b0', backgroundColors: getTempColors())
+    }
+    
+    standardTile("heatLevelUp", "device.switch", canChangeIcon: false, decoration: "flat", width: 2, height: 2) {
+      state("heatLevelUp",   action: "heatLevelUp", icon: "st.thermostat.thermostat-up", backgroundColor: "#F7C4BA")
+    }
+    
+    standardTile("heatLevelDown", "device.switch", canChangeIcon: false, decoration: "flat", width: 2, height: 2) {
+      state("heatLevelDown", action: "heatLevelDown", icon: "st.thermostat.thermostat-down", backgroundColor: "#F7C4BA")
+    }
+    
+    standardTile("switch", "device.switch", canChangeIcon: false, decoration: "flat", width: 2, height: 2) {
+      state("off", label: "Off", action: "switch.on", icon: "st.Weather.weather12", backgroundColor: "#cccccc", nextState: "turningOn")
+      state("turningOn", label: "On...", icon: "st.Weather.weather12", backgroundColor: "#e86d13", nextState: "on")
+      state("on", label: "On", action: "switch.off", icon: "st.Weather.weather12", backgroundColor: "#e86d13", nextState: "turningOff")
+      state("turningOff", label: "Off...", icon: "st.Weather.weather12", backgroundColor: "#cccccc", nextState: "off")
+    }
+    
+    standardTile("refresh", "device.switch", decoration: "flat", width: 2, height: 2) {
+      state("default", action: "refresh.refresh", icon: "st.secondary.refresh")
+    }
+    
+    main("switch")
+    // details(["heatingSetpoint", "heatingSetpointValue", "heatLevelUp", "heatLevelDown", "switch", "refresh"])
+    details([
+      "heatingSetpoint",
+      "switch",
+      "refresh"
+    ])
+  }
 }
 
 def parse(String description) { }
 
 def refresh() {
-	log.debug "refresh"
-	parent.refresh()
+  log.debug "refresh"
+  parent.refresh()
 }
 
 def on() {
-   	parent.setDeviceEnabled(this.device, true)
-    sendEvent(name: "switch", value: "off")
+  log.debug "on"
+  parent.setDeviceEnabled(this.device, true)
+  sendEvent(name: "switch", value: "on")
 }
 
 def off() {
-   	parent.setDeviceEnabled(this.device, false)
-    sendEvent(name: "switch", value: "off")
+  log.debug "off"
+  parent.setDeviceEnabled(this.device, false)
+  sendEvent(name: "switch", value: "off")
 }
 
 def setHeatingSetpoint(Number setPoint) {
-	/*heatingSetPoint = (heatingSetPoint < deviceData.minTemp)? deviceData.minTemp : heatingSetPoint
-	heatingSetPoint = (heatingSetPoint > deviceData.maxTemp)? deviceData.maxTemp : heatingSetPoint
-    */
-   	sendEvent(name: "heatingSetpoint", value: setPoint, unit: "F")
-	parent.setDeviceSetPoint(this.device, setPoint)
+  log.debug setPoint + "\u00b0"
+  /*heatingSetPoint = (heatingSetPoint < deviceData.minTemp)? deviceData.minTemp : heatingSetPoint
+   heatingSetPoint = (heatingSetPoint > deviceData.maxTemp)? deviceData.maxTemp : heatingSetPoint
+   */
+  parent.setDeviceSetPoint(this.device, setPoint)
+  sendEvent(name: "heatingSetpoint", value: setPoint, unit: "F")
 }
 
-def heatLevelUp() { 
-	def setPoint = device.currentValue("heatingSetpoint")
-    setPoint = setPoint + 1
-	setHeatingSetpoint(setPoint)
-}	
+def heatLevelUp() {
+  def setPoint = device.currentValue("heatingSetpoint")
+  setPoint = setPoint + 1
+  setHeatingSetpoint(setPoint)
+}
 
-def heatLevelDown() { 
-	def setPoint = device.currentValue("heatingSetpoint")
-    setPoint = setPoint - 1
-    setHeatingSetpoint(setPoint)
+def heatLevelDown() {
+  def setPoint = device.currentValue("heatingSetpoint")
+  setPoint = setPoint - 1
+  setHeatingSetpoint(setPoint)
 }
 
 def updateDeviceData(data) {
-	sendEvent(name: "heatingSetpoint", value: data.setPoint, unit: "F")
-    sendEvent(name: "switch", value: data.isEnabled ? "on" : "off")
+  sendEvent(name: "heatingSetpoint", value: data.setPoint, unit: "F")
+  sendEvent(name: "switch", value: data.isEnabled ? "on" : "off")
+}
+
+def getTempColors() {
+  def colorMap = [
+    [value: 90,  color: "#f49b88"],
+    [value: 100, color: "#f28770"],
+    [value: 110, color: "#f07358"],
+    [value: 120, color: "#ee5f40"],
+    [value: 130, color: "#ec4b28"],
+    [value: 140, color: "#ea3811"]
+  ]
+  return colorMap
 }
